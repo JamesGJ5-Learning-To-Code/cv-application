db2a089fa2249991d08fd67f0c9ce4bd5bfd37b9
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-31 19:17:40 +0000

    Show components based on state of currentlyEditing

--- cv-project/src/components/Section.js
+++ cv-project/src/components/Section.js
@@ -13,23 +13,38 @@ class Section extends Component {
         }
     }
     render() {
+        const shownComponents = this.selectShownComponents();
         return (
             <div className='Section'>
                 <div>
                     {this.props.sectionName}
                 </div>
-                <EditButton/>
-                {/* TODO: consider passing only the relevant aspects within
-                this.props.details to each of InformationForm and
-                DisplayedInformation */}
-                <InformationForm
-                    details={this.props.details}
-                />
-                <DisplayedInformation
-                    details={this.props.details}
-                />
+                {shownComponents}
             </div>
         );
+    }
+    selectShownComponents() {
+        const shownComponents = (this.state.currentlyEditing)
+            ?
+                this.makeInformationForm()
+            :
+                <div>
+                    <EditButton/>
+                    {this.makeDisplayedInformation()}
+                </div>
+        return shownComponents;
+    }
+    makeInformationForm() {
+        const informationForm = <InformationForm
+            details={this.props.details}
+        />
+        return informationForm;
+    }
+    makeDisplayedInformation() {
+        const displayedInformation = <DisplayedInformation
+            details={this.props.details}
+        />
+        return displayedInformation;
     }
 }
 
