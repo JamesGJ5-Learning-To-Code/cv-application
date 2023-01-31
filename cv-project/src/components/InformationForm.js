d94248a0d1ff3b0d41869fbd34c2aa2af1991e05
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-31 14:27:42 +0000

    Enable each InformationForm to render a SubmitButton

--- cv-project/src/components/InformationForm.js
+++ cv-project/src/components/InformationForm.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import SubmitButton from './SubmitButton';
 
 class InformationForm extends Component {
     render() {
         return (
             <form className='InformationForm'>
+                <SubmitButton/>
             </form>
         )
     }
---
+++ cv-project/src/components/SubmitButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SubmitButton extends Component {
+    render() {
+        return (
+            <button className='SubmitButton'>
+                Submit
+            </button>
+        )
+    }
+}
+
+export default SubmitButton;
