cd30b0b63c584012dd9edcc48ea32adf2c291fee
James Graca-Jones <jamesgracajones@gmail.com>
2023-01-31 12:51:18 +0000

    In App render Sections for each information group

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Section from './components/Section'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Section sectionName='General Information'/>
+        <Section sectionName='Educational Experience'/>
+        <Section sectionName='Practical Experience'/>
       </div>
     );
   }
