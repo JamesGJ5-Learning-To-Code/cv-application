c164650d315d5d1e300d0f4c60ff97bda059ff5c
James Graca-Jones <jamesgracajones@gmail.com>
2023-02-03 12:35:04 +0000

    Rewrite DisplayedDetail in functional style

--- cv-project/src/components/DisplayedDetail.js
+++ cv-project/src/components/DisplayedDetail.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class DisplayedDetail extends Component {
-    render() {
-        return (
-            <div className='DisplayedDetail'>
-                <span>
-                    {this.props.detailHeadingText}
-                </span>
-                {/* The below span will hold the value of the corresponding form
-                control when it is put into state */}
-                <span>
-                    {this.props.detailValue}
-                </span>
-            </div>
-        )
-    }
+const DisplayedDetail = (props) => {
+    return (
+        <div className='DisplayedDetail'>
+            <span>
+                {props.detailHeadingText}
+            </span>
+            {/* The below span will hold the value of the corresponding form
+            control when it is put into state */}
+            <span>
+                {props.detailValue}
+            </span>
+        </div>
+    )
 }
 
 export default DisplayedDetail;
